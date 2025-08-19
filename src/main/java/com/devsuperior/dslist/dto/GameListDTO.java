29f25c53a963034f9d7b363491ec8e426bd78ab3
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-08-19 19:07:28 -0300

    Get Lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.dto;
+
+import java.util.Objects;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList list) {
+		id = list.getId();
+		name = list.getName();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+}
